1ee2bc70b640314b3c075fece5094908c0eb2592
Andrey <takemyenergy55@gmail.com>
2019-05-03 20:05:06 +0500

    add cors

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config, firebase_config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_restful import Api
+from flask_cors import CORS
 import pyrebase
 
 app = Flask(__name__)
@@ -10,6 +11,8 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 api = Api(app)
+cors = CORS(app, resources={r'/*': {'origins': '*'}})
+
 
 firebase = pyrebase.initialize_app(firebase_config)
 
